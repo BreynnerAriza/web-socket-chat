6aa218a9c9b03356a8f7512e4c7bba88c4e2ec00
Breynner Ariza <breynner.fabian.ariza.florez@gmail.com>
2026-04-18 10:30:26 -0500

    Firts commit

---
+++ src/main/java/com/sockets/web/websockertchat/WebSockertChatApplication.java
@@ -0,0 +1,13 @@
+package com.sockets.web.websockertchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSockertChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSockertChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sockets/web/websockertchat/WebSockertChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sockets.web.websockertchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSockertChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
